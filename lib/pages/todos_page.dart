a85fd776d56c8fec265aadaefffe450dd162f0d8
rsyd29 <brasyid15@gmail.com>
2022-05-26 20:26:07 +0700

    Implement method editTodo when onTap widget ListTile

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -239,9 +239,68 @@ class TodoItem extends StatefulWidget {
 }
 
 class _TodoItemState extends State<TodoItem> {
+  late final TextEditingController textController;
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            bool error = false;
+            textController.text = widget.todo.desc;
+
+            return StatefulBuilder(
+                builder: (BuildContext context, StateSetter setState) {
+              return AlertDialog(
+                title: const Text('Edit Todo'),
+                content: TextField(
+                  controller: textController,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    errorText: error ? 'Value cannot be empty' : null,
+                  ),
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('CANCEL'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        error = textController.text.isEmpty ? true : false;
+
+                        if (!error) {
+                          context.read<TodoList>().editTodo(
+                                widget.todo.id,
+                                textController.text.trim(),
+                              );
+                          Navigator.pop(context);
+                        }
+                      });
+                    },
+                    child: const Text('EDIT'),
+                  ),
+                ],
+              );
+            });
+          },
+        );
+      },
       leading: Checkbox(
         value: widget.todo.completed,
         onChanged: (bool? checked) {
