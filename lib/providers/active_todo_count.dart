b67a42e7d67c5ba98ceb9db297181ff4ef081c2b
rsyd29 <brasyid15@gmail.com>
2022-05-26 16:23:14 +0700

    Create widget TodoHeader

--- lib/providers/active_todo_count.dart
+++ lib/providers/active_todo_count.dart
@@ -35,12 +35,14 @@ class ActiveTodoCount with ChangeNotifier {
   ActiveTodoCountState get state => _state;
 
   void update(TodoList todoList) {
+    debugPrint(todoList.state.toString());
     final int newActiveTodoCount = todoList.state.todos
         .where((Todo todo) => !todo.completed)
         .toList()
         .length;
 
     _state = _state.copyWith(activeTodoCount: newActiveTodoCount);
+    debugPrint(state.toString());
     notifyListeners();
   }
 }
