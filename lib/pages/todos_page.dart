a0525db4de9302867d6706eedaae9ae5a3b34a85
rsyd29 <brasyid15@gmail.com>
2022-05-26 16:28:13 +0700

    Create widget CreateTodo

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_provider/providers/active_todo_count.dart';
 
+import '../providers/todo_list.dart';
+
 class TodosPage extends StatefulWidget {
   const TodosPage({Key? key}) : super(key: key);
 
@@ -23,6 +25,7 @@ class _TodosPageState extends State<TodosPage> {
             child: Column(
               children: const [
                 TodoHeader(),
+                CreateTodo(),
               ],
             ),
           ),
@@ -54,6 +57,37 @@ class TodoHeader extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class CreateTodo extends StatefulWidget {
+  const CreateTodo({Key? key}) : super(key: key);
+
+  @override
+  State<CreateTodo> createState() => _CreateTodoState();
+}
+
+class _CreateTodoState extends State<CreateTodo> {
+  final newTodoController = TextEditingController();
+
+  @override
+  void dispose() {
+    newTodoController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: newTodoController,
+      decoration: const InputDecoration(labelText: 'What to do?'),
+      onSubmitted: (String? todoDesc) {
+        if (todoDesc != null && todoDesc.trim().isNotEmpty) {
+          context.read<TodoList>().addTodo(todoDesc);
+          newTodoController.clear();
+        }
+      },
     );
   }
 }
